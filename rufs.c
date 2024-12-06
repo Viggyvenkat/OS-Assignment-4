6756ae897af96af3466dd07dabadd3e0ea01153c
Viggyvenkat <vignesh.v2003@gmail.com>
2024-12-05 21:34:59 -0500

    removed unncessary functions

--- rufs.c
+++ rufs.c
@@ -252,6 +252,23 @@ static int rufs_mkdir(const char *path, mode_t mode) {
 	return 0;
 }
 
+static int rufs_rmdir(const char *path) {
+
+	// Step 1: Use dirname() and basename() to separate parent directory path and target directory name
+
+	// Step 2: Call get_node_by_path() to get inode of target directory
+
+	// Step 3: Clear data block bitmap of target directory
+
+	// Step 4: Clear inode bitmap and its data block
+
+	// Step 5: Call get_node_by_path() to get inode of parent directory
+
+	// Step 6: Call dir_remove() to remove directory entry of target directory in its parent directory
+
+	return 0;
+}
+
 static int rufs_releasedir(const char *path, struct fuse_file_info *fi) {
 	// For this project, you don't need to fill this function
 	// But DO NOT DELETE IT!
@@ -309,6 +326,23 @@ static int rufs_write(const char *path, const char *buffer, size_t size, off_t o
 	return size;
 }
 
+static int rufs_unlink(const char *path) {
+
+	// Step 1: Use dirname() and basename() to separate parent directory path and target file name
+
+	// Step 2: Call get_node_by_path() to get inode of target file
+
+	// Step 3: Clear data block bitmap of target file
+
+	// Step 4: Clear inode bitmap and its data block
+
+	// Step 5: Call get_node_by_path() to get inode of parent directory
+
+	// Step 6: Call dir_remove() to remove directory entry of target file in its parent directory
+
+	return 0;
+}
+
 static int rufs_truncate(const char *path, off_t size) {
 	// For this project, you don't need to fill this function
 	// But DO NOT DELETE IT!
@@ -360,8 +394,6 @@ static struct fuse_operations rufs_ope = {
 
 int main(int argc, char *argv[]) {
 	int fuse_stat;
-	inode_bitmap = calloc((MAX_INUM + 7) / 8, sizeof(unsigned char));
-    data_bitmap = calloc((MAX_DNUM + 7) / 8, sizeof(unsigned char));
 
 	getcwd(diskfile_path, PATH_MAX);
 	strcat(diskfile_path, "/DISKFILE");
